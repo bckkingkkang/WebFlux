bc3b58f59f6e2149bee4589fdf42cdf3905c379a
고가현 <bufar1014@gmail.com>
2024-06-12 11:31:22 +0900

    리액터의 구성 요소 및 용어 정의

--- src/main/java/com/example/kahyun/HelloReactor.java
+++ src/main/java/com/example/kahyun/HelloReactor.java
@@ -1,10 +1,22 @@
 package com.example.kahyun;
 
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 public class HelloReactor {
     public static void main(String[] args) {
         Mono.just("Hello reactor")
                 .subscribe(message -> System.out.println(message));
+
+        // Flux : 데이터를 생성해서 emit 하는 publisher
+        Flux<String> sequence = Flux.just("Hello", "World");
+        // just 메소드 : emit 될 데이터 정의
+        sequence    // operator 체인을 정의해놓은 것
+                .map(data -> data.toUpperCase())
+                // map 메소드 -> 퍼블리셔 쪽에서 에밋된 데이터들을 전달받은 후 대문자로 변환
+                .subscribe(data -> log.info(data));
     }
 }
