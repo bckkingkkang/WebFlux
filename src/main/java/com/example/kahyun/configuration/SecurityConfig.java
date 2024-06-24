467459f6d8cca668023125f19dc26d49cfb93ad7
고가현 <bufar1014@gmail.com>
2024-06-24 10:55:33 +0900

    로그인 화면 config 수정

--- src/main/java/com/example/kahyun/configuration/SecurityConfig.java
+++ src/main/java/com/example/kahyun/configuration/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.example.kahyun.configuration;
 
+import com.example.kahyun.handler.FailureHandler;
+import com.example.kahyun.handler.SuccessHandler;
 import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,22 +11,28 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @EnableWebFluxSecurity
 @Configuration
 public class SecurityConfig {
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
         http
+                .csrf(csrf-> csrf.disable()
+                )
                 .authorizeExchange(request -> request
-                    .pathMatchers("/login/**").permitAll()
+                    .pathMatchers("/user/**").permitAll()
                     .anyExchange().authenticated()
                 )
                 // 로그인 페이지 커스터마이징
                 .formLogin(login -> login
-                        .loginPage("/login")
+                        .loginPage("/user/login")
+                        .requiresAuthenticationMatcher(ServerWebExchangeMatchers.pathMatchers("/user/loginForm"))
                 )
                 .logout(logout -> logout
                         .logoutUrl("/logout")
